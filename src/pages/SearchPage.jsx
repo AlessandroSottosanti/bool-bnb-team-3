c3d83a0d35a215c4a04da13212d4a1f87b1be866
danieleBattistelli <bomber81@gmail.com>
2025-02-12 18:07:45 +0100

    feat: aggiunta stelle su searchPage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 function SearchPage() {
@@ -12,6 +14,20 @@ function SearchPage() {
     const [tipologia, setTipologia] = useState("");
 
     const backEndUrl = import.meta.env.VITE_API_URL;
+
+    //Implemento le stelle per il rating dell'immobile
+    const renderStars = (voto) => {
+        const fullStars = Math.ceil(vote / 2);
+        const emptyStars = 5 - fullStars;
+        const stars = [];
+        for (let i = 0; i < fullStars; i++) {
+            stars.push(<FontAwesomeIcon key={`full-${i}`} icon={faStar} style={{ color: '#ffc107' }} />);
+        }
+        for (let i = 0; i < emptyStars; i++) {
+            stars.push(<FontAwesomeIcon key={`empty-${i}`} icon={faStar} style={{ color: '#e4e5e9' }} />);
+        }
+        return stars;
+    };
 
     const getImmobili = () => {
         axios
@@ -121,7 +137,7 @@ function SearchPage() {
                                         style={{ minHeight: "80px" }}
                                     >
                                         <h5 className="m-0">{immobile.titolo_descrittivo}</h5>
-                                        <span></span>
+                                        {/* <span><p><strong>Voto:</strong> {renderStars(result.vote)}</p></span> */}
                                     </div>
                                     <div className="card-body d-flex flex-column flex-grow-1 text-center">
                                         <p className="flex-grow-1">{immobile.descrizione}</p>

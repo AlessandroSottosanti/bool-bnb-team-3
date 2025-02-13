872545f3b480f51adf4f2eeae1ea1ea350c4ff37
AlessandroSottosanti <asottosanti2@gmail.com>
2025-02-13 14:44:37 +0100

    feat: style

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,9 +1,6 @@
 function AppFooter() {
     return (
-      <footer
-        className="text-center py-2 vh-20 bottom-0 mt-3"
-        style={{ backgroundColor: "#6c757d", color: "black", fontSize: "0.8rem" }}
-      >
+      <footer className="footer text-center text-white py-2 vh-20 bottom-0 mt-3">
         <div className="container">
           <div className="row ">
             {/* Sezione Contatti */}
@@ -15,38 +12,38 @@ function AppFooter() {
             </div>
             {/* Sezione Lavora con noi */}
             <div className="col-md-4 mb-1">
-              <h5 style={{ fontSize: "1rem" }}>Lavora con noi</h5>
+              <h5 className="lavora-con-noi">Lavora con noi</h5>
               <p>
-                <a href="#" className="text-decoration-none text-black">
+                <a href="#" className="text-decoration-none text-white">
                   Posizioni aperte
                 </a>
               </p>
               <p>
-                <a href="#" className="text-decoration-none text-black">
+                <a href="#" className="text-decoration-none text-white">
                   Invia il tuo CV
                 </a>
               </p>
               <p>
-                <a href="#" className="text-decoration-none text-black">
+                <a href="#" className="text-decoration-none text-white">
                   Sedi lavorative
                 </a>
               </p>
             </div>
             {/* Sezione Link Utili */}
             <div className="col-md-4 mb-1">
-              <h5 style={{ fontSize: "1rem" }}>Link Utili</h5>
+              <h5 className="link-utili">Link Utili</h5>
               <p>
-                <a href="#" className="text-decoration-none text-black">
+                <a href="#" className="text-decoration-none text-white">
                   Chi siamo
                 </a>
               </p>
               <p>
-                <a href="#" className="text-decoration-none text-black">
+                <a href="#" className="text-decoration-none text-white">
                   Privacy Policy
                 </a>
               </p>
               <p>
-                <a href="#" className="text-decoration-none text-black">
+                <a href="#" className="text-decoration-none text-white">
                   Termini e Condizioni
                 </a>
               </p>

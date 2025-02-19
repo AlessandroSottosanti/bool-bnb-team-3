db4f3513e36fd8b9a4a35b0755572cbb1291deee
AlessandroSottosanti <asottosanti2@gmail.com>
2025-02-19 15:45:06 +0100

    feat: button up

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import AppHeader from "./AppHeader";
 import AppFooter from "./AppFooter";
+import ScrollToTopButton from "./ScrollToTopButton";
 
 function AppLayout() {
     return (
         <>
         <AppHeader/>
         <Outlet/>
+        <ScrollToTopButton/>
         <AppFooter/>
         </>
     )
